bee0d68236e074f1f91fc7cae534e700b062fa41
Matthias Rolke <mr.amtrack@gmail.com>
2019-02-22 19:31:53 +0100

    [WIP]: refactor: plugins
    [ci skip]

--- src/commands/browserforce/apply.ts
+++ src/commands/browserforce/apply.ts
@@ -22,45 +22,37 @@ export default class BrowserforceApply extends BrowserforceCommand {
 
   // tslint:disable-next-line:no-any
   public async run(): Promise<any> {
-    const logger = await core.Logger.root();
+    // const logger = await core.Logger.root();
     this.ux.log(
       `Applying plan file ${
         this.flags.definitionfile
       } to org ${this.org.getUsername()}`
     );
     for (const setting of this.settings) {
-      const driver = setting.Driver.default;
-      const instance = new driver(this.bf, this.org);
-      this.ux.startSpinner(`[${driver.name}] retrieving state`);
-      let state;
-      try {
-        state = await instance.retrieve(setting.value);
-      } catch (err) {
-        this.ux.stopSpinner('failed');
-        throw err;
-      }
-      this.ux.stopSpinner();
-      logger.debug(`generating action for driver ${driver.name}`);
-      const action = instance.diff(state, setting.value);
-      this.ux.stopSpinner();
-      if (action && Object.keys(action).length) {
-        this.ux.startSpinner(
-          `[${driver.name}] ${Object.keys(action)
-            .map(key => {
-              return `changing '${key}' to '${JSON.stringify(action[key])}'`;
-            })
-            .join('\n')}`
-        );
-        try {
-          await instance.apply(action);
-        } catch (err) {
-          this.ux.stopSpinner('failed');
-          throw err;
-        }
-        this.ux.stopSpinner();
-      } else {
-        this.ux.log(`[${driver.name}] no action necessary`);
-      }
+      // const driver = setting.Driver.default;
+      // let state;
+      await setting.Driver.default.retrieve([setting.value]);
+      // logger.debug(`generating action for driver ${driver.name}`);
+      // const action = instance.diff(state, setting.value);
+      // this.ux.stopSpinner();
+      // if (action && Object.keys(action).length) {
+      //   this.ux.startSpinner(
+      //     `[${driver.name}] ${Object.keys(action)
+      //       .map(key => {
+      //         return `changing '${key}' to '${JSON.stringify(action[key])}'`;
+      //       })
+      //       .join('\n')}`
+      //   );
+      //   try {
+      //     await instance.apply(action);
+      //   } catch (err) {
+      //     this.ux.stopSpinner('failed');
+      //     throw err;
+      //   }
+      //   this.ux.stopSpinner();
+      // } else {
+      //   this.ux.log(`[${driver.name}] no action necessary`);
+      // }
     }
     return { success: true };
   }
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,59 @@
-import { core } from '@salesforce/command';
+import { core, SfdxCommand, UX } from '@salesforce/command';
 import * as jsonMergePatch from 'json-merge-patch';
 import Browserforce from './browserforce';
 
-export abstract class BrowserforcePlugin {
-  protected org: core.Org;
+export abstract class BrowserforcePlugin extends SfdxCommand {
+
+  protected static requiresUsername = true;
+
+  static retrieve = async function (this, argv?: string[], opts?) {
+    if (!argv) argv = process.argv.slice(2);
+    let cmd = new this(argv, null)
+    return cmd._retrieve();
+  }
+
   protected browserforce: Browserforce;
 
+  protected definition;
+  protected plan;
+
   public constructor(browserforce: Browserforce, org: core.Org) {
+    super([], null);
     this.browserforce = browserforce;
-    this.org = org;
   }
+
+  public async init() {
+    console.log('init');
+    this.logger = await core.Logger.child(this.statics.name);
+    this.ux = new UX(this.logger, true);
+    this.ux.log('init from plugin');
+    console.log('hi');
+    // assign org and browserforce
+    // assign definition
+  }
+
+  public async run() {
+    console.log('run');
+    throw new Error('BrowserforcePlugin should not be run directly');
+  }
+
+  public async _retrieve() {
+    console.log('_retrieve');
+    await this.init();
+    this.ux.startSpinner(`[${this.constructor.name}] retrieving state`);
+    this.plan = await this.retrieve(this.definition);
+    this.ux.stopSpinner();
+    return this.plan;
+  }
+
+  public async _apply() {
+    return await this.apply(this.plan);
+  }
+
+  public async catch(e) {
+    console.error(e);
+  }
+
   // tslint:disable-next-line:no-any
   public abstract async retrieve(definition?): Promise<any>;
   public diff(state, definition) {
